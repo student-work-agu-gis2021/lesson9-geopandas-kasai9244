a3256cb64e19d2a61f8a4742ee068bc1999ad572
kasai9244 <>
2021-06-07 03:57:39 +0000

    I added the code to print answers.

--- Exercise_9_problem_3.py
+++ Exercise_9_problem_3.py
@@ -90,7 +90,9 @@ movements.head()
 #  - What was the maximum distance travelled in meters?
 
 # YOUR CODE HERE 6 to find max, min,mean of the distance.
-
+print(mean(movements['distance']))
+print(max(movements['distance'].dropna()))
+print(min(movements['distance']))
 # - Finally, save the movements of into a Shapefile called ``some_movements.shp``
 
 # YOUR CODE HERE 7 to save as Shapefile
