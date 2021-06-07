a59e066e1d7f9e47bdfa6288b6307cea57103726
kasai9244 <>
2021-06-07 03:38:48 +0000

    I added the code to read in the shapefile as a geodataframe called data and to reproject the data from WGS84 projection into EPSG:32735 -projection which stands for UTM Zone 35S to transform the data into metric system.

--- Exercise_9_problem_2.py
+++ Exercise_9_problem_2.py
@@ -11,7 +11,9 @@ import pandas as pd
 import geopandas as gpd
 from shapely.geometry import Point
 # YOUR CODE HERE 1 to read data
-data = None
+data = pd.read_csv("data/some_posts.csv")
+
+
 
 
 # CODE FOR TESTING YOUR SOLUTION
--- Exercise_9_problem_3.py
+++ Exercise_9_problem_3.py
@@ -10,7 +10,8 @@
 import geopandas as gpd
 from pyproj import CRS
 data=None
-
+data=gpd.read_file("Kruger_posts.shp")
+data=data.to_crs(epsg=32735)
 # - Check the crs of the input data. If this information is missing, set it as epsg:4326 (WGS84).
 # - Reproject the data from WGS84 to `EPSG:32735` -projection which stands for UTM Zone 35S (UTM zone for South Africa) to transform the data into metric system. (don't create a new variable, update the existing variable `data`!)"
 
